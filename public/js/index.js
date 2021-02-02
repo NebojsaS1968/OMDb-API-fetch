15d1a724873e179a8547d89f3c48add1e2d0203f
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-02-02 12:17:55 +0100

    autocomplete list on input

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 const input = document.getElementById('filmTitle')
+const matchList = document.getElementById('match-list')
 
 const searchFilms = async (searchText) => {
     const url = '../data/films.json'
@@ -16,15 +17,42 @@ const searchFilms = async (searchText) => {
         return title.match(regex)
     })
 
+    // Clear HTML and array when text in the input reaches 0
     if(searchText.length === 0){
         matches = []
+        matchList.style.visibility = 'hidden'
     }
 
-    console.log(matches)
-    //outputHtml(matches)
+    matchList.addEventListener('click', () => {
+        clickMatch(matches)
+    })
+
+    //console.log(matches)
+    outputHtml(matches)
 }
 
-//const outputHtml = (matches) => {
+const outputHtml = (matches) => {
+    if(matches.length > 0){
+        const html = matches.map(match => `
+            <div class="auto">
+                <p>${match.imdb250}</p>
+            </div>
+        `).join('')
+        matchList.innerHTML = html
+    }
+
+}
+
+// PROBLEM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const clickMatch = (matches) => {
+    matches.forEach(match => {
+        input.value = match.imdb250
+    })
+    matchList.style.visibility = 'hidden'
+}
 
 
-input.addEventListener('input', () => searchFilms(input.value))
+input.addEventListener('input', () => {
+    searchFilms(input.value)
+    matchList.style.visibility = 'visible'
+})
